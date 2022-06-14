82837e9a7c1d196babfcb0ae5214fc0200e18425
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-06-14 09:06:49 +0300

    basic structure, material-ui

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Autocomplete } from "@react-google-maps/api"
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material"
+import SearchIcon from "@mui/icons-material/Search"
+
+const Header = () => {
+  
+  return (
+    <AppBar position="static">
+     
+    </AppBar>
+  )
+}
+
+export default Header
